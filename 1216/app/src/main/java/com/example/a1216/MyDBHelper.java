b28b39898e1ddf8733a49a96d2786e8d58184754
chooou <irenechou91@gmail.com>
2022-12-16 11:01:20 +0800

    123

---
+++ 1216/app/src/main/java/com/example/a1216/MyDBHelper.java
@@ -0,0 +1,25 @@
+package com.example.a1216;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static final String name = "mDatebase.db";
+    private static final int version=1;
+    public MyDBHelper(@Nullable Context context){
+        super(context,name, null,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
